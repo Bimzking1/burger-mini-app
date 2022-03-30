426b0099db5e84236c2823d8d2e0ab6d7a79ae40
Bimzking1 <kingofbimo@gmail.com>
2022-03-30 21:26:34 +0700

    Rabu, 30 Maret 2022 - Part 1 Fixes #7

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import { Routes, Route, Link } from 'react-router-dom'
 // Components
 import Home from './Pages/Home'
 import BurgerMaker from './Pages/BurgerMaker';
+import Layout from './Layouts/Layout';
 
 function App() {
   return (
     <>
-      <h1>INI DEFAULT LAYOUT</h1> {/* Defaut Layout tidak ikut berubah mengikuti routing */}
+      {/*
+      <h1>INI DEFAULT LAYOUT</h1>
+      // Defaut Layout tidak ikut berubah mengikuti routing
+      
       <Link to="/"><p>Home</p></Link>
       <Link to="/burger-maker"><p>Burger Maker</p></Link>
-
-      <Routes>   {/* Hanya element didalam Routes yang ikut berubah mengikuti routing */}
-        <Route path="/" element={<Home/>}/>
-        <Route path="/burger-maker" element={<BurgerMaker/>}/>
-      </Routes>
+      */}
+      <Layout>
+        <Routes>   {/* Hanya element didalam Routes yang ikut berubah mengikuti routing */}
+          <Route path="/" element={<Home/>}/>
+          <Route path="/burger-maker" element={<BurgerMaker/>}/>
+        </Routes>
+      </Layout>
     </>
   );
 }
--- src/Components/Burger/Bun.js
+++ src/Components/Burger/Bun.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import style from './burger.module.css'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  background-color: white;
+  padding-left: 0px;
+`
 
 const Bun = (props) => {
 
@@ -17,7 +23,22 @@ const Bun = (props) => {
   }
 
   return (
-    <div className={`${style.bun} ${bunClass}`}></div>
+    // a. Default styling (melalui CSS)
+    // <div className={`${style.bun} ${bunClass}`}></div>
+
+    // b. Inline class styling
+    // <div className={`${style.bun} ${bunClass}`} style={{ paddingLeft: '0px' }}></div>
+
+    // c. Styled components
+    // (Install dulu dengan npm install styled-components)
+
+    <div className={`${style.bun} ${bunClass}`}>
+      {/* <button> */}
+      {/* <StyledButton>
+        Button
+      </StyledButton> */}
+      {/* </button> */}
+    </div>
   )
 }
 
